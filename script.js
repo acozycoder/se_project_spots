f10207412f6890c7b885b5b3930f465de4e91ab7
Alicia McKinney <amckin810@gmail.com>
2025-01-29 16:03:16 -0600

    Add JS file

---
+++ script.js
@@ -0,0 +1,31 @@
+const initialCards = [
+  objectOne: {
+    name: "Home Office Goals",
+    link: https://unsplash.com/photos/two-black-computer-monitors-on-black-table-p-xSl33Wxyc
+  }
+
+  objectTwo: {
+    name: "Financial Freedom",
+    link: https://unsplash.com/photos/100-us-dollar-bill-xYaMK5p3vCA
+  }
+
+  objectThree: {
+    name: "Frequent Travels",
+    link: https://unsplash.com/photos/person-holding-passports-0QcSnCM0aMc
+  }
+
+  objectFour: {
+    name: "Reliable Vehicle",
+    link: https://unsplash.com/photos/person-holding-on-black-steering-wheel-inside-car-rOLKpojjbGM
+  }
+
+  objectFive: {
+    name: "Home Ownership",
+    link: https://unsplash.com/photos/gray-padded-chaise-couch-beside-window-rEJxpBskj3Q
+  }
+
+  objectSix: {
+    name: "Dream Career",
+    link: https://unsplash.com/photos/woman-in-black-top-using-surface-laptop-glRqyWJgUeY
+  }
+]
