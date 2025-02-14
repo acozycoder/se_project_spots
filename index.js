bd18de5448eaea3148ff7870764fcc1d60f98625
Alicia McKinney <amckin810@gmail.com>
2025-02-13 19:02:57 -0600

    Add card template

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const initialCards = [
   {
     name: "Home Office Goals",
-    link: "https://unsplash.com/photos/two-black-computer-monitors-on-black-table-p-xSl33Wxyc"
+    link: "https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
     name: "Financial Freedom",
-    link: "https://unsplash.com/photos/100-us-dollar-bill-xYaMK5p3vCA"
+    link: "https://images.unsplash.com/photo-1596313398625-2c16b75031b3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
     name: "Frequent Travels",
-    link: "https://unsplash.com/photos/person-holding-passports-0QcSnCM0aMc"
+    link: "https://images.unsplash.com/photo-1553697388-94e804e2f0f6?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
     name: "Reliable Vehicle",
-    link: "https://unsplash.com/photos/person-holding-on-black-steering-wheel-inside-car-rOLKpojjbGM"
+    link: "https://images.unsplash.com/photo-1497564245203-66a1216f073a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
     name: "Home Ownership",
-    link: "https://unsplash.com/photos/gray-padded-chaise-couch-beside-window-rEJxpBskj3Q"
+    link: "https://images.unsplash.com/photo-1564078516393-cf04bd966897?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 
   {
     name: "Dream Career",
-    link: "https://unsplash.com/photos/woman-in-black-top-using-surface-laptop-glRqyWJgUeY"
+    link: "https://images.unsplash.com/photo-1573164713988-8665fc963095?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   },
 ];
 
@@ -38,6 +38,21 @@ const profileModal = document.querySelector("#edit-modal");
 const profileForm = profileModal.querySelector(".modal__form");
 const profileModalName = profileModal.querySelector("#modal-name");
 const profileModalDescription = profileModal.querySelector("#modal-description");
+
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+  const cardNameElement = cardElement.querySelector(".card__title");
+  const cardImageElement = cardElement.querySelector(".card__image");
+
+  cardNameElement.textContent = data.name;
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+
+  return cardElement;
+}
 
 function openModal() {
   profileModalName.value = profileName.textContent;
@@ -63,3 +78,8 @@ function submitProfile(evt) {
 }
 
 profileForm.addEventListener("submit", submitProfile);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
